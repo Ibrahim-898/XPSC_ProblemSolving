25ea27d1232b4b989de0c6eecd128fe734fe84a5
MdIbrahimKhalilulla <117997065+Ibrahim-898@users.noreply.github.com>
2024-11-09 23:38:44 +0600

    Add files via upload
    Day7

---
+++ Week2/Day7/A_Set.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define ld  long double
+#define ios() ios_base:: sync_with_stdio(0);cin.tie(0);
+bool cmp(const pair<int,int> &a,const pair<int,int> &b){
+    if(a.first!=b.first)
+    return (a.first<b.first);
+    else
+     return (a.second>b.second);
+}
+class compare {
+public:
+    bool operator()(pair<int, int> a, pair<int, int> b) {
+        if (a.second == b.second) {
+            return a.first > b.first;
+        }
+        return a.second < b.second;
+    }
+};
+int fact(int n)
+{
+    return (n==1) ? 1: n * fact(n - 1); 
+    // ? :
+}
+const int N=100;
+void solve() {
+    ll l,r,k;
+    cin >> l >> r >> k;
+    ll res = r/k-l+1;
+    if(res<0) cout << 0 << endl;
+    else
+    cout << res << endl;
+    
+    
+}
+int main()
+{
+    ios();
+    int  t = 1;
+   cin >> t;
+    while(t--){
+        solve();
+
+    }
+}
