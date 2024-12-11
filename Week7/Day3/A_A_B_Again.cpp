9836d12f1878d09f38da690df8e5f0eaa20ad0c8
MdIbrahimKhalilulla <117997065+Ibrahim-898@users.noreply.github.com>
2024-12-11 23:30:30 +0600

    Add files via upload
    Day3

---
+++ Week7/Day3/A_A_B_Again.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define ld  long double
+#define ios() ios_base:: sync_with_stdio(0);cin.tie(0);
+bool cmp(const pair<int,int> &a,const pair<int,int> &b) {
+    if(a.first!=b.first)
+        return (a.first<b.first);
+    else
+        return (a.second>b.second);
+}
+int fact(int n)
+{
+    return (n==1) ? 1: n * fact(n - 1);
+    // ?
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, sum=0;
+        cin >> n;
+        while(n>0) {
+        sum+=n%10;
+        n/=10;
+        }
+        cout << sum << endl;
+    }
+}
